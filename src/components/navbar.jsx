159733532350df0dea1a7b0c96462225f959cd18
rahfianugerah <nrahfia@gmail.com>
2024-09-18 23:48:21 +0700

    feat: create navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,82 @@
+'use client'
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+
+export default function MyNavbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    { label: "Home", href: "#home" },
+    { label: "Projects", href: "#projects" },
+    { label: "Profile", href: "#profile" },
+    { label: "Blog", href: "#blog" },
+    { label: "Contact", href: "#contact" }
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <p className="navbar-link text-lg text-inherit">Rahfi Anugerah</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link className="navbar-link" color="foreground" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="navbar-link" color="foreground" href="#">
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="navbar-link" color="foreground" href="#">
+            Profile
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#"><FaGithub color="#ffffff"/></Link>
+        </NavbarItem>
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#"><FaLinkedin color="#ffffff"/></Link>
+        </NavbarItem>
+        <NavbarItem className="navbar-link">
+          <Button as={Link} color="white" href="#" variant="ghost">
+            My Blog
+          </Button>
+        </NavbarItem>
+        <NavbarItem className="navbar-link">
+          <Button as={Link} color="primary" href="#" variant="shadow">
+            Contact
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={index}>
+            <Link
+              color="foreground"
+              className="navbar-link w-full"
+              href={item.href}
+              size="lg"
+            >
+              {item.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
