e94fc4f0cc84787221d8ff0fc69d5446a97f6dae
rahfianugerah <nrahfia@gmail.com>
2024-09-19 15:55:35 +0700

    feat: add the hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 
 import Footer from "@/components/footer";
+import Hero from "@/components/hero";
 import MyNavbar from "@/components/navbar";
 
 export default function Home() {
@@ -8,6 +9,9 @@ export default function Home() {
       <header>
         <MyNavbar/>
       </header>
+      <main>
+        <Hero/>
+      </main>
       <Footer/>
     </>
   );
