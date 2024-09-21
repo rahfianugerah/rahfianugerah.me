a617cf2a4fcbda2ee7678adaea80afc0b6c03765
rahfianugerah <nrahfia@gmail.com>
2024-09-21 09:45:03 +0700

    feat: add NextUI components
    Add skeleton and card components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|link|navbar|ripple|spinner).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|card|link|navbar|skeleton|ripple|spinner).js"
   ],
   theme: {
     extend: {
